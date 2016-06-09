d1c0865f836cd5725d2a5547e166a8431e4383f9
Vitor Travassos Castelo Branco <vtcb@ug2c21.windows.cin.ufpe.br>
2016-06-09 13:27:10 -0300

    Add Predict Method

--- LinearRegression.py
+++ LinearRegression.py
@@ -8,7 +8,7 @@ class LinearRegression(object):
         self.weights  = np.random.random( [features] ) / 100
         self.bias     = np.random.random( [1] ) / 100
 
-    def fit(self, X, Y):
+    def fit(self, X, Y, **kwargs):
         # X [samples, features]
         # Y [samples]
 
@@ -23,6 +23,19 @@ class LinearRegression(object):
                 self.weights[j] = self.weights[j] + self.alpha * (y - h) * x[j]
 
             self.bias = self.bias + self.alpha * (y - h)
+
+    def predict(self, X):
+        return np.array( [
+            self.h_t(X[i])
+            for i in range(len(X))
+        ] )
+
+        # print X[0]
+        # return np.fromfunction(
+        #     lambda i: self.h_t(X[i]),
+        #     [len(X)],
+        #     dtype=int
+        # )
 
     def h_t(self, x):
         return self.bias + np.dot(self.weights, x)
@@ -39,8 +52,15 @@ def main():
         [2]
     ] )
 
+    X_test = np.array( [
+        [3],
+        [4]
+    ] )
+
     for i in range(1000):
         lr.fit(X, Y)
+
+    print(lr.predict(X_test))
 
     print("")
     print(lr.weights)
